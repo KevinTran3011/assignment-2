e8e05c49978dc5584ab6867e5b90fa135fbcdcd1
Kevin <kevin@Linux.myguest.virtualbox.org>
2023-10-01 00:06:30 +1000

    added in the demo API functions

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -1,29 +1,66 @@
 const db = require("../models");
 const Contacts = db.contacts;
 const Phones = db.phones;
-const Op = db.Sequelize.Op;
+const Op = db.Sequelize.Op; //Op is basically where clause
 
 // Create contact
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
+
+    try{
+        const newContact = await Contacts.create(req.body);
+        res.json(newContact)
+    }
+    catch(err){
+        res.json({message: 'Error while creating contact: ' + err})
+    }
+    
     
 };
 
 // Get all contacts
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
+
+    try{
+        const contactList = await Contacts.findAll();
+        res.json(contactList)
+
+    } catch(err){
+        res.json({message: 'Error getting contacts: ' + err})
+
+
+    }
     
 };
 
 // Get one contact by id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
   
 };
 
 // Update one contact by id
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
 
 // Delete one contact by id
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -1,28 +1,57 @@
 const db = require("../models");
 const Phones = db.phones;
-const Op = db.Sequelize.Op;
+const Op = db.Sequelize.Op; //Op is basically where clause
 
 // Create phone
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
 
 // Get all phones
-exports.findAll = (req, res) => {
+exports.findAll = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
 
 // Get one phone by id
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
   
+    try{
+
+    } catch(err){
+        
+    }
 };
 
 // Update one phone by id
-exports.update = (req, res) => {
+exports.update = async(req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
 
 // Delete one phone by id
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
+
+    try{
+
+    } catch(err){
+        
+    }
     
 };
